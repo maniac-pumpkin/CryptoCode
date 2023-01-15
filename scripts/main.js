0facd65d810ee90081978708370d236c21545cda
Mohammad Mehdi Beheshti <shadymx@yahoo.com>
2023-01-16 01:53:15 +0330

    Some updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
-// LogIn Form
+// Navigation
+(navigation = () => {
+	const hamMenu = document.querySelector(".ham-nav");
+	const hamBTN = document.querySelector(".hamburger-menu");
+	const navLogOutBTN = document.querySelector(".ham-nav-logo__logout-btn");
+	const logOutBTN = document.querySelector(".nav-list__logout-btn");
+
+	hamBTN.addEventListener("click", () => {
+		hamBTN.classList.toggle("ham-dash--toggle");
+		hamMenu.classList.toggle("ham-nav--show");
+	});
+
+	navLogOutBTN.addEventListener("click", () => {
+		window.open("../index.html", "_self");
+	});
+
+	logOutBTN.addEventListener("click", () => {
+		window.open("../index.html", "_self");
+	});
+})();
